82a15db3483697946f3479f5b01f1fcb2d70e5d5
Igoralexandre02 <igoralexandrevieira652@gmail.com>
2026-03-28 22:29:11 -0400

    feat: adicao venda e compra.

---
+++ database/migrations/2026_03_29_021601_create_vendas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vendas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('empresa_id')->constrained(table: 'empresas')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained(table: 'users')->onUpdate('cascade')->onDelete('cascade');
+            $table->decimal('valor_total');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vendas');
+    }
+};
