77b3f45f71266a9c1786ce8d17ca86a34207bb07
Igoralexandre02 <igoralexandrevieira652@gmail.com>
2026-03-29 08:51:53 -0400

    feat: adicionado Produto e Movimentacao_Financeiras.

--- database/migrations/2026_03_29_124144_create_produtos_table.php
+++ database/migrations/2026_03_29_124144_create_produtos_table.php
@@ -4,17 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('users_empresas', function (Blueprint $table) {
+        Schema::create('produtos', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained(table: 'users')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('empresa_id')->constrained(table: 'empresas')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreignId('perfil_id')->constrained(table: 'perfils')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('nome', 255);
+            $table->decimal('preco_venda');
+            $table->integer('estoque_atual');
+            $table->integer('estoque_minimo')->nullable();
             $table->timestamps();
         });
     }
@@ -24,7 +27,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users_empresas');
+        Schema::dropIfExists('produtos');
     }
 };
- 
