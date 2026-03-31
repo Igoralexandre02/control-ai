3f57ef54f1dbe734b96b8cd00d842c892f22c1e6
Igoralexandre02 <igoralexandrevieira652@gmail.com>
2026-03-30 20:10:22 -0400

    montando template.

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="text-white text-6xl">Dashboard</div>
+@endsection
---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+    <div>
+        Edit-User
+    </div>
+@endsection
